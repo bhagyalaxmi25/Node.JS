a958994abce91564884854e2e788425ed04acec0
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-08-02 23:48:53 +0530

    node.js

---
+++ import.js
@@ -0,0 +1,2 @@
+import {a, b, d} from "./mymodules.js"
+console.log(a, b, d);
--- mymodules.js
+++ mymodules.js
@@ -1,5 +1,9 @@
-export const a = 1
-export const b = 2
-export const c = 3
-export const d = 4
-export const e = 5
+// export is two type:-
+// 1)named export
+// 2)default export
+
+export const a = 1    //named export 
+export const b = 2    //named export
+export const c = 3    //named export
+export const d = 4    //named export
+export const e = 5    //named export
