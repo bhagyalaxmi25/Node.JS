48d6ea8f487b168422978b4ccc9d9f823dce0e9f
bhagyalaxmi25 <palaibhagyalaxmi81@gmail.com>
2024-08-03 21:35:57 +0530

    nodejs

--- import.js
+++ import.js
@@ -1,2 +1,17 @@
-import {a, b, d} from "./mymodules.js"
-console.log(a, b, d);
+// import {a, b, d} from "./mymodules.js"
+// console.log(a, b, d)
+
+
+// import obj from "./mymodules.js"
+// console.log(obj)
+
+// const a = require("./mymodule2.js")
+// console.log(a)
+
+// (function(exports,require,module,__filename,__dirname){
+    // Module code actually lives here
+// })
+
+const a = require("./mymodule2.js")
+
+console.log(a,__dirname,__filename)
